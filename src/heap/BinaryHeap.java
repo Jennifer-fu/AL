ccbc1fa1f735497a6fb3dab88deceec4225d996c
twer <twer@twers-MacBook-Pro.local>
2012-11-06 23:04:56 +0800

    build min heap

--- src/heap/BinaryHeap.java
+++ src/heap/BinaryHeap.java
@@ -1,15 +1,36 @@
 package heap;
 
+import java.util.ArrayList;
+
 public class BinaryHeap {
 
 
     public int[] buildMinHeap(int[] array) {
-        if (array.length == 1) return array;
-        if (array[0] > array[1]) {
-            int temp = array[0];
-            array[0] = array[1];
-            array[1] = temp;
+        //n=2*i+1 or n=2*i+2
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            heapify(array, i);
         }
         return array;
+    }
+
+    private void heapify(int[] array, int i) {
+        int min = i;
+        int left = i * 2 + 1;
+        int right = i * 2 + 2;
+
+        if (left < array.length && array[i] > array[left]) {
+            min = left;
+        }
+
+        if (right < array.length && min > array[right]) {
+            min = right;
+        }
+
+        if (min != i) {
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+            heapify(array, min);
+        }
     }
 }
--- test/heap/BinaryHeapTest.java
+++ test/heap/BinaryHeapTest.java
@@ -7,19 +7,36 @@ import static org.junit.Assert.assertThat;
 
 public class BinaryHeapTest {
     @Test
-    public void should_return_the_element_if_only_one_element_to_build_binary_heap(){
+    public void should_return_the_element_if_only_one_element_to_build_binary_heap() {
         BinaryHeap heap = new BinaryHeap();
         int[] result = heap.buildMinHeap(new int[]{1});
 
-        assertThat(result,is(new int[]{1}));
+        assertThat(result, is(new int[]{1}));
     }
 
     @Test
-    public void should_return_1_2_if_array_is_2_1(){
+    public void should_return_1_2_if_array_is_2_1() {
         BinaryHeap heap = new BinaryHeap();
         int[] result = heap.buildMinHeap(new int[]{2, 1});
 
-        assertThat(result,is(new int[]{1,2}));
+        assertThat(result, is(new int[]{1, 2}));
     }
+
+    @Test
+    public void should_return_1_3_2_if_array_is_3_2_1() {
+        BinaryHeap heap = new BinaryHeap();
+        int[] result = heap.buildMinHeap(new int[]{3, 1, 2});
+
+        assertThat(result, is(new int[]{1, 3, 2}));
+    }
+
+    @Test
+    public void should_return_10_17_20_30_38_30_24_34_if_array_is_10_30_20_34_38_30_24_17() {
+        BinaryHeap heap = new BinaryHeap();
+        int[] result = heap.buildMinHeap(new int[]{10, 30, 20, 34, 38, 30, 24, 17});
+
+        assertThat(result, is(new int[]{10, 17, 20, 30, 38, 30, 24, 34}));
+    }
+
 
 }
