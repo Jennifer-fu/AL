2cefe9c4edef51bca3afc06e8b94b1075a363574
twer <twer@twers-MacBook-Pro.local>
2012-11-07 22:06:45 +0800

    min heap sort

--- src/heap/BinaryHeap.java
+++ src/heap/BinaryHeap.java
@@ -1,36 +1,49 @@
 package heap;
 
-import java.util.ArrayList;
-
 public class BinaryHeap {
 
-
-    public int[] buildMinHeap(int[] array) {
-        //n=2*i+1 or n=2*i+2
-        for (int i = array.length / 2 - 1; i >= 0; i--) {
-            heapify(array, i);
+    public void sort(int[] array) {
+        buildMinHeap(array);
+        int heapSize = array.length;
+        for (int i = heapSize - 1; i > 0; i--) {
+            swap(array, 0, i);
+            heapSize--;
+            heapify(array, 0, heapSize);
         }
-        return array;
     }
 
-    private void heapify(int[] array, int i) {
+    public void buildMinHeap(int[] array) {
+        //n=2*i+1 or n=2*i+2
+        int heapSize = array.length;
+        for (int i = heapSize / 2 - 1; i >= 0; i--) {
+            heapify(array, i, heapSize);
+        }
+    }
+
+    private void heapify(int[] array, int i, int heapSize) {
         int min = i;
         int left = i * 2 + 1;
         int right = i * 2 + 2;
 
-        if (left < array.length && array[i] > array[left]) {
+        if (left < heapSize && array[i] > array[left]) {
             min = left;
         }
 
-        if (right < array.length && min > array[right]) {
+        if (right < heapSize && array[min] > array[right]) {
             min = right;
         }
 
         if (min != i) {
-            int temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
-            heapify(array, min);
+            swap(array, i, min);
+            heapify(array, min, heapSize);
         }
     }
+
+    private void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+
 }
--- test/heap/BinaryHeapTest.java
+++ test/heap/BinaryHeapTest.java
@@ -9,34 +9,46 @@ public class BinaryHeapTest {
     @Test
     public void should_return_the_element_if_only_one_element_to_build_binary_heap() {
         BinaryHeap heap = new BinaryHeap();
-        int[] result = heap.buildMinHeap(new int[]{1});
+        int[] array = {1};
+        heap.buildMinHeap(array);
 
-        assertThat(result, is(new int[]{1}));
+        assertThat(array, is(new int[]{1}));
     }
 
     @Test
     public void should_return_1_2_if_array_is_2_1() {
         BinaryHeap heap = new BinaryHeap();
-        int[] result = heap.buildMinHeap(new int[]{2, 1});
+        int[] array = {2, 1};
+        heap.buildMinHeap(array);
 
-        assertThat(result, is(new int[]{1, 2}));
+        assertThat(array, is(new int[]{1, 2}));
     }
 
     @Test
     public void should_return_1_3_2_if_array_is_3_2_1() {
         BinaryHeap heap = new BinaryHeap();
-        int[] result = heap.buildMinHeap(new int[]{3, 1, 2});
+        int[] array = {3, 1, 2};
+        heap.buildMinHeap(array);
 
-        assertThat(result, is(new int[]{1, 3, 2}));
+        assertThat(array, is(new int[]{1, 3, 2}));
     }
 
     @Test
     public void should_return_10_17_20_30_38_30_24_34_if_array_is_10_30_20_34_38_30_24_17() {
         BinaryHeap heap = new BinaryHeap();
-        int[] result = heap.buildMinHeap(new int[]{10, 30, 20, 34, 38, 30, 24, 17});
+        int[] array = {10, 30, 20, 34, 38, 30, 24, 17};
+        heap.buildMinHeap(array);
 
-        assertThat(result, is(new int[]{10, 17, 20, 30, 38, 30, 24, 34}));
+        assertThat(array, is(new int[]{10, 17, 20, 30, 38, 30, 24, 34}));
     }
 
+    @Test
+    public void should_return_sorted_array(){
+        BinaryHeap heap = new BinaryHeap();
+        int[] array = {16,14,10,8,7,9,3,2,4,1};
+        heap.sort(array);
+
+        assertThat(array,is(new int[]{16,14,10,9,8,7,4,3,2,1}));
+    }
 
 }
